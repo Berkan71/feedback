f79e16bbd2517d5def74874c4bd9366bb59ebcb0
berkanilkizmaz <berkanil.kizmaz@cfn.com.tr>
2022-02-27 21:45:07 +0300

    Feedback app first commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 5,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+  },
+  {
+    id: 2,
+    rating: 2,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+  },
+  {
+    id: 3,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+  },
+];
+
+export default FeedbackData;
