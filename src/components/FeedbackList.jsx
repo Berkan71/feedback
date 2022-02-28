ffb61dcef868988445cc576c609e26dc633c413c
berkanilkizmaz <berkanil.kizmaz@cfn.com.tr>
2022-02-28 15:13:31 +0300

    Rating and average added

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import FeedbackItem from './FeedbackItem'
+import React from "react";
+import PropTypes from "prop-types";
+import FeedbackItem from "./FeedbackItem";
 
-function FeedbackList({feedback}) {
-
-  if(!feedback || feedback.length === 0) {
-    return <p>No Feedback Yet</p>
+function FeedbackList({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback Yet</p>;
   }
 
   return (
-    <div className='feedback-list'>
+    <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem
+          key={item.id}
+          item={item}
+          handleDelete={handleDelete}
+        />
       ))}
     </div>
-  )
+  );
 }
 
-export default FeedbackList
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+export default FeedbackList;
